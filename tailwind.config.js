5f36fae174ae29bed55fba027add51f1346a485a
Brian Hogg <brian@hoggworks.com>
2021-09-16 11:45:47 -0400

    Initial single-file tailwind stuff

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './assets/**/*.js',
+    './layout/**/*.liquid',
+    './sections/**/*.liquid',
+    './snippets/**/*.liquid',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
