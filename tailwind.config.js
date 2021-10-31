682ce3ad7dd14921028815e6bc799796401aaecf
Dayton Pereira <daytonp@gmail.com>
2021-10-31 16:10:38 -0400

    used grid to setup the footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,7 +43,7 @@ module.exports = {
         '3/4screen' : '74vw',
       },
       boxShadow:{
-        button : "0 0 0 0.2rem rgba(0, 0, 0, 1)",
+        button : "0 0 0 0.08rem rgba(0, 0, 0, 1)",
         button_inner : "inset 0 0 0 0.2rem rgba(0, 0, 0, 1)",
       },
       colors: {
